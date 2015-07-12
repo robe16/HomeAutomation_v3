538da228ceb4acc29975119dce661c2ecbef7a79
robe16 <robe16_dev@outlook.com>
2015-07-12 22:13:33 +0100

    General commit

--- src/port_listener.py
+++ src/port_listener.py
@@ -1,27 +1,35 @@
 import socket
 
-# creating a socket object
-s = socket.socket(socket.AF_INET,
-                  socket.SOCK_STREAM)
+def listen():
+    # creating a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# get local Host machine name
-host = socket.gethostname()
-port = 1616
+    # bind to pot
+    #s.bind((socket.gethostname(), 1616))
+    s.bind(('localhost', 1616))
 
-# bind to pot
-s.bind((host, port))
+    # Que up to 5 requests
+    s.listen(5)
 
-# Que up to 5 requests
-s.listen(5)
-
-while True:
-    # establish connection
-    (clientSocket, address) = s.accept()
-    data = clientSocket.recv()
-    #
-    print("got a connection from: " + str(address))
-    print("message as follows: " + data)
-    #
-    STRresponse=""
-    clientSocket.send(STRresponse.encode('ascii'))
-    #clientSocket.close()
+    while True:
+        # establish connection
+        (connection, client_address) = s.accept()
+        #
+        try:
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(4096)
+                #
+                print("----")
+                print("received a connection from client: " + str(client_address))
+                print("message as follows: " + data)
+                print("----")
+                #
+                if data:
+                    STRresponse = "Test response"
+                    connection.sendall(STRresponse)
+                else:
+                    break
+        finally:
+            # Clean up the connection
+            connection.close()
