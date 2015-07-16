49d8c7f059b106827c6b310acc7b192ed00e318b
robe16 <robe16_dev@outlook.com>
2015-07-16 23:34:15 +0100

    Update to tv and tivo object creation code

---
+++ src/create_objects.py
@@ -0,0 +1,16 @@
+from object_tv_lg import object_LGTV
+from object_tivo import object_TIVO
+
+
+def create_lgtv(STRloungetv_lgtv_ipaddress, STRpairingkey=False):
+    if not STRpairingkey:
+        return object_LGTV(STRloungetv_lgtv_ipaddress, 8080, STRpairingkey=STRpairingkey)
+    else:
+        return object_LGTV(STRloungetv_lgtv_ipaddress, 8080)
+
+
+def create_tivo(STRloungetv_tivo_ipaddress, STRaccesskey=False):
+    if not STRaccesskey:
+        return object_TIVO(STRloungetv_tivo_ipaddress, 31339, STRaccesskey=STRaccesskey)
+    else:
+        return object_TIVO(STRloungetv_tivo_ipaddress, 31339)
