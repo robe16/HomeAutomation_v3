0e588cf4d4617baf6d7027e68045bf93491accbb
robe16 <robe16_dev@outlook.com>
2015-07-18 11:24:28 +0100

    Minor bug fixes

--- src/tests/test_bottle2.py
+++ src/tests/test_bottle2.py
@@ -2,19 +2,19 @@ from src.bottle import route, run
 
 @route('/structure/')
 def structure_json():
-    return "Details of the structure"
+    return "Details of the structure\n"
 
 @route('/room/<name>')
-def get_devices(name=None):
-    return "List of devices in %s" % name
+def get_devices(name="-"):
+    return "List of devices in %s\n" % (name)
 
 @route('/device/<name>/<device>/<command>')
-def send_command(name=None, device=None, command=None):
-    return "Command will be sent to specific device\nRoom: %s\nDevice: %s\nCommand: %s" % name, device, command
+def send_command(name="-", device="-", command="-"):
+    return "Command will be sent to specific device\nRoom: %s\nDevice: %s\nCommand: %s\n" % (name, device, command)
 
 @route('/device/<name>/<device>/<command>')
-def get_infocommand(name=None, device=None, command=None):
-    return "Command will be sent to specific device\nRoom: %s\nDevice: %s\nCommand: %s" % name, device, command
+def get_infocommand(name="-", device="-", command="-"):
+    return "Command will be sent to specific device\nRoom: %s\nDevice: %s\nCommand: %s\n" % (name, device, command)
 
 # @route('/recipes/<name>', method='DELETE' )
 # def recipe_delete( name="Mystery Recipe" ):
@@ -26,7 +26,7 @@ def get_infocommand(name=None, device=None, command=None):
 
 run(host='localhost', port=8080, debug=True)
 
-# curl -X GET http://localhost:8080/structure
+# curl -X GET http://localhost:8080/structure/
 # curl -X GET http://localhost:8080/room/lounge
 # curl -X GET http://localhost:8080/device/lounge/lgtv/volup
 # curl -X GET http://localhost:8080/device/lounge/lgtv/channels
