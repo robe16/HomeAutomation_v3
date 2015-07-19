d6150afa4f22296ad71b58f0d1bae9bdfff41af1
robe16 <robe16_dev@outlook.com>
2015-07-19 22:56:47 +0100

    API call for channel images rectified and tested

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import dataholder
 from config import read_config
 import create_objects
 from src.bottle import route, run, static_file
+import os
 from object_tv_lg import object_LGTV
 from object_tivo import object_TIVO
 
@@ -17,19 +18,16 @@ def __init__():
 @route('/device/<room>/<device>/<command>')
 def send_command(room="-", device="-", command="-"):
     #TODO
-    if room=="lounge":
-        if device=="lgtv":
-            return object_LGTV.sendCmd(command)
-        elif device=="tivo":
-            return object_TIVO.sendCmd(command)
-        else:
-            return "Error"
+    if room=="lounge" and device=="lgtv":
+        return object_LGTV.sendCmd(command)
+    elif room=="lounge" and device=="tivo":
+        return object_TIVO.sendCmd(command)
     else:
         return "Error"
 
 @route('/image/<category>/<filename:re:.*\.png>')
 def send_image(category, filename):
-    return static_file(filename, root='./img/%s' % category, mimetype='image/png')
-
+    root = os.path.join(os.path.dirname(__file__), '..', 'img/%s' % category)
+    return static_file(filename, root=root, mimetype='image/png')
 
 run(host='localhost', port=8080, debug=True)
