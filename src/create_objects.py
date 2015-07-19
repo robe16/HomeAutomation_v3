ae35dbcc65a1770a54b6e9e425bd01277f7144a8
robe16 <robe16_dev@outlook.com>
2015-07-19 10:02:52 +0100

    Amended code to be easier to use by other defs

--- src/create_objects.py
+++ src/create_objects.py
@@ -2,15 +2,15 @@ from object_tv_lg import object_LGTV
 from object_tivo import object_TIVO
 
 
-def create_lgtv(STRloungetv_lgtv_ipaddress, STRpairingkey=False):
-    if not STRpairingkey:
+def create_lgtv(STRloungetv_lgtv_ipaddress, STRpairingkey):
+    if not STRpairingkey==None:
         return object_LGTV(STRloungetv_lgtv_ipaddress, 8080, STRpairingkey=STRpairingkey)
     else:
         return object_LGTV(STRloungetv_lgtv_ipaddress, 8080)
 
 
-def create_tivo(STRloungetv_tivo_ipaddress, STRaccesskey=False):
-    if not STRaccesskey:
+def create_tivo(STRloungetv_tivo_ipaddress, STRaccesskey):
+    if not STRaccesskey==None:
         return object_TIVO(STRloungetv_tivo_ipaddress, 31339, STRaccesskey=STRaccesskey)
     else:
         return object_TIVO(STRloungetv_tivo_ipaddress, 31339)
