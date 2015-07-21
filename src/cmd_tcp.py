899e7c833e667aef1cafa2b2b811b155fdc34c38
robe16 <robe16_dev@outlook.com>
2015-07-21 18:56:32 +0100

    Work started on telnet code

--- src/cmd_tcp.py
+++ src/cmd_tcp.py
@@ -1,4 +1,10 @@
-def sendTCP():
-    '''Send data via tcp to IP address over network conection'''
-    #do something
-    return False
+import telnetlib
+
+def sendTCP(ipaddress, data):
+    # Send data via tcp to IP address over network conection
+    try:
+        tn = telnetlib.Telnet(ipaddress)
+        tn.write(data + '\n')
+        return True
+    except Exception:
+        return False
