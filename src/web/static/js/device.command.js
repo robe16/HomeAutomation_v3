85669927c96528425d783d7c68eb2b5520f250c3
robe16 <robe16_dev@outlook.com>
2015-08-08 16:30:35 +0100

    Further work on web interface and bug fix for showing and hiding alert box

--- src/web/static/js/device.command.js
+++ src/web/static/js/device.command.js
@@ -1,18 +1,15 @@
 function sendCommand(cmd) {
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", cmd, false);
-    xmlHttp.send(null);
-
-    if (xmlHttp.responseText=200){
-        document.getElementById("alert").innerHTML = "The command has been successfully sent";
-        document.getElementById("alert").style["display"] = "inline";
-        document.getElementById("alert").className = "alert alert-success";
-        (document.getElementById("alert").style["display"] = "none").delay(3000);
-    } else {
-        document.getElementById("alert").innerHTML = "<strong>Error!!</strong> The command has not been successful being sent to the device";
-        document.getElementById("alert").style["display"] = "inline";
-        document.getElementById("alert").className = "alert alert-danger";
-        (document.getElementById("alert").style["display"] = "none").delay(3000);
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", cmd, false);
+        xmlHttp.send(null);
+        var v = document.getElementById('alert')
+        if (xmlHttp.responseText=200){
+            v.innerHTML = "The command has been successfully sent";
+            v.setAttribute('class', 'alert alert-success');
+        } else {
+            v.innerHTML = "<strong>Error!!</strong> The command has not been successful being sent to the device";
+            v.setAttribute('class', 'alert alert-danger');
+        }
+        v.style.visibility="visible";
+        setTimeout(function(){v.style.visibility="hidden"}, 2000);
     }
-}
