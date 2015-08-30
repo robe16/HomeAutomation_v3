e286ccdc105318747fcf1bf37b4e07127cca84de
robe16 <robe16_dev@outlook.com>
2015-08-30 13:01:11 +0100

    Improvements to web interface to allow for more dynamic page creation dependant on user configurations

--- src/web_createpages.py
+++ src/web_createpages.py
@@ -6,10 +6,26 @@ def create_home():
            urlopen('web/index.html').read().encode('utf-8')+\
            urlopen('web/footer.html').read().encode('utf-8')
 
-def create_device_group(listings):
+def create_device_group(listings, LSTobjects):
+    x=0
+    tvguide_device=False
+    chan_array_no=None
+    STRdevicehtml=""
+    while x<len(LSTobjects):
+        if LSTobjects[x].getLogo:
+            STRpanel=("<img src=\"/img/logo/{}\" style=\"height:25px;\"/> {}").format(LSTobjects[x].getLogo(), LSTobjects[x].getName())
+        else:
+            STRpanel=LSTobjects[x].getName()
+        STRobjhtml=urlopen(('web/{}').format(LSTobjects[x].getHtml())).read().encode('utf-8')
+        STRdevicehtml+=urlopen('web/comp_panel.html').read().encode('utf-8').format(STRpanel, STRobjhtml)
+        STRdevicehtml+="<br>"
+        if LSTobjects[x].getTvguide_use:
+            tvguide_device=LSTobjects[x].getGroup()+"/"+LSTobjects[x].getDevice()
+            chan_array_no=LSTobjects[x].getChan_array_no()
+        x+=1
     return _header()+\
-           urlopen('web/alert.html').read().encode('utf-8')+\
-           urlopen('web/loungetv.html').read().encode('utf-8').format(_lgtv(), _tivo(), _listings_html(listings, "lounge/tivo", chan_array_no=0))+\
+           urlopen('web/comp_alert.html').read().encode('utf-8')+\
+           urlopen('web/loungetv.html').read().encode('utf-8').format(STRdevicehtml, _listings_html(listings, tvguide_device, chan_array_no=chan_array_no))+\
            urlopen('web/footer.html').read().encode('utf-8')
 
 def create_tvguide(listings):
@@ -19,13 +35,13 @@ def create_tvguide(listings):
 
 def create_settings_rooms():
     return _header()+\
-           urlopen('web/alert.html').read().encode('utf-8')+\
+           urlopen('web/comp_alert.html').read().encode('utf-8')+\
            urlopen('web/settings_rooms.html').read().encode('utf-8')+\
            urlopen('web/footer.html').read().encode('utf-8')
 
 def create_settings_devices():
     return _header()+\
-           urlopen('web/alert.html').read().encode('utf-8')+\
+           urlopen('web/comp_alert.html').read().encode('utf-8')+\
            urlopen('web/settings_devices.html').read().encode('utf-8')+\
            urlopen('web/footer.html').read().encode('utf-8')
 
@@ -33,7 +49,7 @@ def create_settings_nest(clientID, STRnest_pincode, random):
     nesturl = ("https://home.nest.com/login/oauth2?client_id=" + clientID + "&state=" + "26GA-" + random)
     pincode = ("value=\""+STRnest_pincode+"\"") if bool(STRnest_pincode) else ""
     return _header()+\
-           urlopen('web/alert.html').read().encode('utf-8')+\
+           urlopen('web/comp_alert.html').read().encode('utf-8')+\
            urlopen('web/settings_nest.html').read().encode('utf-8').format(nesturl, pincode)+\
            urlopen('web/footer.html').read().encode('utf-8')
 
@@ -48,13 +64,7 @@ def _header():
 def _headerdrops():
     return urlopen('web/header_dropdown.html').read().encode('utf-8')
 
-def _lgtv():
-    return urlopen('web/loungetv-lgtv.html').read().encode('utf-8')
-
-def _tivo():
-    return urlopen('web/loungetv-tivo.html').read().encode('utf-8')
-
-def _listings_html (listings, device, chan_array_no=False):
+def _listings_html (listings, device, chan_array_no=-1):
     if listings:
         return urlopen('web/tvguide-data.html').read().encode('utf-8').format(_listings(listings, device, chan_array_no))
     else:
@@ -78,7 +88,7 @@ def _listingsrow(x, item, device, chan_array_no):
         color="#e8e8e8"
     else:
         color="#ffffff"
-    if device:
+    if device and not chan_array_no==-1:
         go = urlopen('web/tvguide-row_go.html').read().encode('utf-8').format(device, item[4][chan_array_no])
     else:
         go = ""
