fafea0788cffb729a30fae55bc6653f2615b9f36
robe16 <robe16_dev@outlook.com>
2016-02-05 00:00:06 +0000

    Further work on device settings UI. Device selection popup updated for better design and interaction.

--- src/web_settings_devices.py
+++ src/web_settings_devices.py
@@ -8,24 +8,20 @@ def _settings_devices():
 
 def _settings_devices_selection():
     #
-    body = ''
+    body = '<div class="row">'
     count = 0
     devices = [['LG TV', 'logo_lg.png'],
                ['Virgin Media', 'logo_virgin.png'],
                ['Xbox One', 'logo_xboxone.png'],
-               ['Nest Account', 'logo_nest_blue.png'],
-               ['Test 1', 'logo_other.png'],
-               ['Test 2', 'logo_other.png'],
-               ['Test 3', 'logo_other.png'],
-               ['Test 4', 'logo_other.png'],
-               ['Test 5', 'logo_other.png'],
-               ['Test 6', 'logo_other.png']]
+               ['Nest Account', 'logo_nest_blue.png']]
     #
     for dvc in devices:
-        if count % 4 == 0:
-            body += '<br>'
-        body += urlopen('web/html_settings/devices/settings_devices_selection_item.html').read().encode('utf-8').format(name = dvc[0],
-                                                                                                                        img = dvc[1])
+        if count> 0 and count % 4 == 0:
+            body += '</div><div class="row">'
+        body += urlopen('web/html_settings/settings_devices_selection_item.html').read().encode('utf-8').format(name = dvc[0],
+                                                                                                                img = dvc[1])
         count += 1
+    #
+    body += '</div>'
     #
     return body
