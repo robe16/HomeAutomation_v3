90b9c2d55fb1db83e8d5e5fb7c95fa3b77cb6a46
robe16 <robe16_dev@outlook.com>
2016-11-06 20:20:53 +0000

    Print function moved to own seperate def - TODO note added for passed through msg to be saved to log/audit file at future date.

--- src/console_messages.py
+++ src/console_messages.py
@@ -3,7 +3,7 @@ from datetime import datetime
 timeformat = '%d/%m/%Y %H:%M:%S.%f'
 
 def print_command(command, dvc_or_acc_id, device_type, dvc_ip, response):
-    print ("{timestamp} - {dvc_or_acc_id} - \'{command}\' request sent to {device_type}{dvc_ip} - {response}".format(
+    _print("{timestamp} - {dvc_or_acc_id} - \'{command}\' request sent to {device_type}{dvc_ip} - {response}".format(
         timestamp=datetime.now().strftime(timeformat),
         dvc_or_acc_id=dvc_or_acc_id,
         command=command.replace('/r',''),
@@ -13,7 +13,7 @@ def print_command(command, dvc_or_acc_id, device_type, dvc_ip, response):
 
 
 def print_channelbuild(num_current, num_total, name_chan):
-    print ('{timestamp} - Building channels and retrieving TV Listing information: {current} out of {total} - {name}'
+    _print('{timestamp} - Building channels and retrieving TV Listing information: {current} out of {total} - {name}'
            .format(timestamp = datetime.now().strftime(timeformat),
                    current = num_current,
                    total = num_total,
@@ -23,13 +23,13 @@ def print_channelbuild(num_current, num_total, name_chan):
 def print_error(error_msg, dvc_or_acc_id=''):
     if (dvc_or_acc_id):
         dvc_or_acc_id += ' - '
-    print ('{timestamp} - {dvc_or_acc_id}ERROR: {error}'.format(timestamp = datetime.now().strftime(timeformat),
+    _print('{timestamp} - {dvc_or_acc_id}ERROR: {error}'.format(timestamp = datetime.now().strftime(timeformat),
                                                                 dvc_or_acc_id=dvc_or_acc_id,
                                                                 error = error_msg))
 
 
 def print_http(httpcode, error_msg):
-    print ('{timestamp} - HTTP {httpcode}: {error}'.format(timestamp = datetime.now().strftime(timeformat),
+    _print('{timestamp} - HTTP {httpcode}: {error}'.format(timestamp = datetime.now().strftime(timeformat),
                                                            httpcode = httpcode,
                                                            error = error_msg))
 
@@ -37,6 +37,11 @@ def print_http(httpcode, error_msg):
 def print_msg(msg, dvc_or_acc_id=''):
     if (dvc_or_acc_id):
         dvc_or_acc_id += ' - '
-    print ('{timestamp} - {dvc_or_acc_id}{msg}'.format(timestamp=datetime.now().strftime(timeformat),
+    _print('{timestamp} - {dvc_or_acc_id}{msg}'.format(timestamp=datetime.now().strftime(timeformat),
                                                        dvc_or_acc_id=dvc_or_acc_id,
                                                        msg=msg))
+
+
+def _print(msg):
+    # TODO - code to add msg to log file
+    print(msg)
