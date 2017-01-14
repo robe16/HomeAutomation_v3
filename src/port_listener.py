8289d726a834ad5b133dd84b65160751a86e1b64
Rob <robe16@hotmail.co.uk>
2017-01-14 00:18:21 +0000

    Update to cross domain request to wildcard (*) due to 'file://' location of files.

--- src/port_listener.py
+++ src/port_listener.py
@@ -32,9 +32,9 @@ def start_bottle(_devices, _accounts, _infoservices):
 
 def enable_cors(response):
     #
-    # Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    # Wildcard '*' for Access-Control-Allow-Origin as mirrorUI will be hosted in 'file://' on device
     #
-    response.headers['Access-Control-Allow-Origin'] = 'localhost'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET'
     # response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
     #
