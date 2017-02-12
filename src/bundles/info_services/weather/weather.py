4cb153c78189a3acdf2b909bd6d7859c16898843
Rob <robe16@hotmail.co.uk>
2017-02-12 00:45:43 +0000

    Sunrise and sunset data added to weather info-service

--- src/bundles/info_services/weather/weather.py
+++ src/bundles/info_services/weather/weather.py
@@ -2,6 +2,7 @@ from bundles.info_services.info_service import InfoService
 from log.console_messages import print_error
 import json
 from data_source_metoffice import createForecast
+from data_source_sunrise_sunset_org import createSunriseSet
 
 
 class info_metoffice(InfoService):
@@ -22,5 +23,18 @@ class info_metoffice(InfoService):
     def getForecast(self):
         #
         forecast = createForecast()
+        #
+        lat = forecast['location']['latitude']
+        lng = forecast['location']['longitude']
+        #
+        day = 0
+        while day < len(forecast['days']):
+            #
+            forecast['days'][day]['sunRiseSet'] = createSunriseSet(forecast['days'][day]['date'],
+                                                                   lat,
+                                                                   lng)
+            #
+            day += 1
+            #
         #
         return forecast
