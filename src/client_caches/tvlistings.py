a5ec5ee5774ba6ed2de9838ae1feccae8b671b60
Rob <robe16@hotmail.co.uk>
2017-04-02 16:23:00 +0100

    Removal of excess code.

--- src/client_caches/tvlistings.py
+++ src/client_caches/tvlistings.py
@@ -1,11 +1,9 @@
 import ast
-# from config.users.config_users import get_usernames
 
 
 def compile_tvlistings():
     #
     data = {}
-    # data['users'] = get_usernames()
     #
     try:
         return ast.literal_eval(data)
