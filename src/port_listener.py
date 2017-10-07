00014c993873402d6b91d446ef2ae4b4cba7ed5f
Rob <robe16@hotmail.co.uk>
2017-10-07 11:33:15 +0100

    Updated so better logs are kept for bottle operation on port listening and responses.

--- src/port_listener.py
+++ src/port_listener.py
@@ -6,7 +6,7 @@ from bottle import request, run, static_file, HTTPResponse
 
 from config.bindings.config_bindings import get_cfg_group_seq, get_cfg_thing_seq, get_cfg_info_seq
 from lists.resources.english import *
-from log.log import log_error
+from log.log import log_error, log_general
 
 ################################################################################################
 
@@ -39,6 +39,17 @@ def enable_cors(response):
     return response
 
 ################################################################################################
+# Create log message
+################################################################################################
+
+def log_msg(request, resource):
+    #
+    ip = request['REMOTE_ADDR']
+    msg = '{ip} - {resource}'.format(ip=ip, resource=resource)
+    #
+    return msg
+
+################################################################################################
 # Provision of server config for clients
 ################################################################################################
 
@@ -48,34 +59,41 @@ from client_caches.tvchannels import compile_tvchannels
 
 @get(uri_cache_setup)
 def cache_setup():
+    log = log_msg(request, uri_cache_setup)
     try:
+        log_general(log)
         return HTTPResponse(body=compile_setup(), status=200)
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         return HTTPResponse(status=404)
 
 
 @get(uri_cache_users)
 def cache_users():
+    log = log_msg(request, uri_cache_setup)
     try:
+        log_general(log)
         return HTTPResponse(body=compile_users(), status=200)
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         return HTTPResponse(status=404)
 
 
 @get(uri_cache_tvchannels)
 def cache_tvchannels():
+    log = log_msg(request, uri_cache_setup)
     try:
+        log_general(log)
         return HTTPResponse(body=compile_tvchannels(), status=200)
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         return HTTPResponse(status=404)
 
 
 # TODO: details of cache update subscription caught here - now to set so any update to respective jsons will inform client
 @post(uri_cache_subscribe)
 def cache_subscribe():
+    log = log_msg(request, uri_cache_setup)
     try:
         #
         r = request
@@ -84,9 +102,10 @@ def cache_subscribe():
         ipaddress = request.json['ipaddress']
         port = request.json['port']
         #
+        log_general(log)
         return HTTPResponse(status=200)
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         return HTTPResponse(status=404)
 
 
@@ -98,11 +117,14 @@ from config.users.config_users import check_pin
 
 @post(uri_user_pin)
 def user_checkpin():
+    log = log_msg(request, uri_cache_setup)
     check = check_pin(request.json['user'],
                       request.json['pin'])
     if check:
+        log_general('{log} - {user} - {passfail}'.format(log=log, user=request.json['user'], passfail='pass'))
         return HTTPResponse(status=200)
     else:
+        log_general('{log} - {user} - {passfail}'.format(log=log, user=request.json['user'], passfail='fail'))
         return HTTPResponse(status=401)
 
 
@@ -115,14 +137,18 @@ def get_data_infoservice(service=False, resource_requested=False):
     #
     global infoservices
     #
+    log = log_msg(request, uri_cache_setup)
+    #
     try:
         #
         if (not service) or (not resource_requested):
+            log_error('{log} - {error}'.format(log=log, error='URI invalid'))
             raise HTTPError(404)
         #
         try:
             info_seq = get_cfg_info_seq(service)
         except Exception as e:
+            log_error('{log} - {error}'.format(log=log, error=e))
             raise HTTPError(404)
         #
         data_dict = {'data': resource_requested}
@@ -148,10 +174,11 @@ def get_data_infoservice(service=False, resource_requested=False):
             else:
                 response.status=400
         #
+        log_general(log)
         return response
         #
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         raise HTTPError(500)
 
 
@@ -160,20 +187,26 @@ def get_data_device(group=False, thing=False, resource_requested=False):
     #
     global devices
     #
+    log = log_msg(request, uri_cache_setup)
+    #
     try:
         #
         if (not group) or (not thing) or (not resource_requested):
+            log_error('{log} - {error}'.format(log=log, error='URI invalid'))
             raise HTTPError(404)
         #
         try:
             group_seq = get_cfg_group_seq(group)
             thing_seq = get_cfg_thing_seq(group, thing)
         except Exception as e:
+            log_error('{log} - {error}'.format(log=log, error=e))
             raise HTTPError(404)
         #
         data_dict = {'data': resource_requested}
         #
         rsp = devices[group_seq][thing_seq].getData(data_dict)
+        #
+        log_general(log)
         #
         if isinstance(rsp, bool):
             return HTTPResponse(status=200) if rsp else HTTPResponse(status=400)
@@ -181,7 +214,7 @@ def get_data_device(group=False, thing=False, resource_requested=False):
             return HTTPResponse(body=str(rsp), status=200) if bool(rsp) else HTTPResponse(status=400)
         #
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         raise HTTPError(500)
 
 
@@ -194,20 +227,26 @@ def send_command_device(group=False, thing=False):
     #
     global devices
     #
+    log = log_msg(request, uri_cache_setup)
+    #
     try:
         #
         if (not group) or (not thing):
+            log_error('{log} - {error}'.format(log=log, error='URI invalid'))
             raise HTTPError(404)
         #
         try:
             group_seq = get_cfg_group_seq(group)
             thing_seq = get_cfg_thing_seq(group, thing)
         except Exception as e:
+            log_error('{log} - {error}'.format(log=log, error=e))
             raise HTTPError(404)
         #
         cmd_dict = request.json
         #
         rsp = devices[group_seq][thing_seq].sendCmd(cmd_dict)
+        #
+        log_general(log)
         #
         if isinstance(rsp, bool):
             return HTTPResponse(status=200) if rsp else HTTPResponse(status=400)
@@ -215,7 +254,7 @@ def send_command_device(group=False, thing=False):
             return HTTPResponse(body=str(rsp), status=200) if bool(rsp) else HTTPResponse(status=400)
         #
     except Exception as e:
-        log_error('{error}'.format(error=e))
+        log_error('{log} - {error}'.format(log=log, error=e))
         raise HTTPError(500)
 
 
@@ -241,17 +280,31 @@ def send_command_device(group=False, thing=False):
 
 @get(uri_favicon)
 def send_favicon():
-    root = os.path.join(os.path.dirname(__file__), 'imgs/logo')
-    return static_file('favicon.ico', root=root)
+    log = log_msg(request, uri_cache_setup)
+    try:
+        root = os.path.join(os.path.dirname(__file__), 'imgs/logo')
+        log_general(log)
+        return static_file('favicon.ico', root=root)
+    except Exception as e:
+        log_error('{log} - {error}'.format(log=log, error=e))
+        raise HTTPError(500)
 
 
 @get(uri_image)
 def get_image(category, filename):
-    root = os.path.join(os.path.dirname(__file__), 'imgs/{img_cat}'.format(img_cat=category))
-    mimetype = filename.split('.')[1]
-    return static_file(filename, root=root, mimetype='image/{mimetype}'.format(mimetype=mimetype))
+    log = log_msg(request, uri_cache_setup)
+    try:
+        root = os.path.join(os.path.dirname(__file__), 'imgs/{img_cat}'.format(img_cat=category))
+        mimetype = filename.split('.')[1]
+        log_general(log)
+        return static_file(filename, root=root, mimetype='image/{mimetype}'.format(mimetype=mimetype))
+    except Exception as e:
+        log_error('{log} - {error}'.format(log=log, error=e))
+        raise HTTPError(500)
 
 ################################################################################################
 
 def run_bottle(self_port):
-    run(host='0.0.0.0', port=self_port, debug=True)
+    host='0.0.0.0'
+    log_general('Bottle started: listening on {host}:{port}'.format(host=host, port=self_port))
+    run(host=host, port=self_port, debug=True)
